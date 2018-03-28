385bf8dad9562af09356d2a48b38dfacc4aa1f36
Siddhartha Basu <biosidd@gmail.com>
2018-03-28 02:41:52 +0000

    Added validate package

---
+++ validate/validate.go
@@ -0,0 +1,19 @@
+package validate
+
+import (
+	"fmt"
+
+	"gopkg.in/urfave/cli.v1"
+)
+
+func ValidateServerArgs(c *cli.Context) error {
+	for _, p := range []string{"dictycontent-pass", "dictycontent-db", "dictycontent-user"} {
+		if len(c.String(p)) == 0 {
+			return cli.NewExitError(
+				fmt.Sprintf("argument %s is missing", p),
+				2,
+			)
+		}
+	}
+	return nil
+}
