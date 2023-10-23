fc75e2820fccb5ea61f87d918203ebe39eef34d5
Siddhartha Basu <biosidd@gmail.com>
2023-10-23 14:52:23 -0500

    feat(repository): add ContentRepository interface to define methods for interacting with content data
    The ContentRepository interface is added to define methods for
    retrieving, adding, editing, and deleting content data. This interface
    will be implemented by concrete repository implementations to provide
    the necessary functionality for interacting with the content data.

---
+++ internal/repository/repository.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"github.com/dictyBase/go-genproto/dictybaseapis/content"
+	"github.com/dictyBase/modware-content/internal/model"
+)
+
+type ContentRepository interface {
+	GetContentBySlug(id string) (*model.ContentDoc, error)
+	GetContent(id string) (*model.ContentDoc, error)
+	AddContent(cnt *content.NewContentAttributes) (*model.ContentDoc, error)
+	EditContent(
+		cnt *content.ExistingContentAttributes,
+	) (*model.ContentDoc, error)
+	DeleteContent(id string) error
+}
