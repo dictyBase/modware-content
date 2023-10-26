160c7497b878fadb841eb0732739d043263cd508
Siddhartha Basu <biosidd@gmail.com>
2023-10-26 14:54:16 -0500

    feat(arangodb): add implementation for NewContentRepo function in arangodb package
    The NewContentRepo function in the arangodb package is implemented. It
    creates a new instance of the arangorepository struct and initializes
    its session, database, and content fields. It also establishes a
    connection to the ArangoDB database using the provided connection
    parameters. The function then finds or creates a collection in the
    database with the specified name and schema options. If successful, it
    returns the arangorepository instance. Otherwise, it returns an error.
    This implementation allows the arangodb package to provide a concrete
    implementation of the ContentRepository interface defined in the
    internal/repository package. It enables the application to interact with
    the ArangoDB database for content-related operations.

---
+++ internal/repository/arangodb/arangodb.go
@@ -0,0 +1,44 @@
+package arangodb
+
+import (
+	"fmt"
+
+	driver "github.com/arangodb/go-driver"
+	manager "github.com/dictyBase/arangomanager"
+	"github.com/dictyBase/modware-content/internal/model"
+	"github.com/dictyBase/modware-content/internal/repository"
+)
+
+type arangorepository struct {
+	sess     *manager.Session
+	database *manager.Database
+	content  driver.Collection
+}
+
+func NewContentRepo(
+	connP *manager.ConnectParams,
+	collection string,
+) (repository.ContentRepository, error) {
+	arp := &arangorepository{}
+	sess, dbs, err := manager.NewSessionDb(connP)
+	if err != nil {
+		return arp, fmt.Errorf("error in getting new session %s", err)
+	}
+	arp.sess = sess
+	arp.database = dbs
+	schemaOptions = &driver.CollectionSchemaOptions{}
+	schemaOptions.LoadRule(model.Schema())
+	contentCollection, err := dbs.FindOrCreateCollection(
+		collection,
+		&driver.CreateCollectionOptions{Schema: schemaOptions},
+	)
+	if err != nil {
+		return arp, fmt.Errorf(
+			"error in finding or creating collection %s",
+			err,
+		)
+	}
+	arp.content = contentCollection
+
+	return arp, nil
+}
