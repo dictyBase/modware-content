f7c94c86ec1d920f4facdb6028418ed9b0a9ed45
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-10-30 15:30:41 -0500

    feat(arangodb): add ContentInsert statement to handle inserting content into the database
    The ContentInsert statement is added to handle inserting content into
    the database. This statement includes the necessary fields such as name,
    slug, namespace, created_by, updated_by, content, created_on, and
    updated_on. The statement uses the INSERT INTO syntax to insert the
    values into the content_collection and returns the newly inserted
    document.

--- internal/repository/arangodb/statement.go
+++ internal/repository/arangodb/statement.go
@@ -7,4 +7,17 @@ const (
 			LIMIT 1
 			RETURN cnt
 	`
+
+	ContentInsert = `
+		INSERT {
+			name: @name,
+			slug: @slug,
+			namespace: @namespace,
+			created_by: @created_by,
+			updated_by: @updated_by,
+			content: @content,
+			created_on : DATE_ISO8601(DATE_NOW()),
+			updated_on : DATE_ISO8601(DATE_NOW()),
+		} INTO @content_collection RETURN NEW
+	`
 )
