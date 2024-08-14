c22b44ea5c9ec93436a84b0038cfe5d63c443707
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-08-14 12:54:02 -0500

    refactor(arangodb_test.go): move ListContents tests to separate file
    The content listing tests, including pagination and validation,
    are moved from `arangodb_test.go` to this new file, ensuring that each
    test file has a clear and focused responsibility.

---
+++ internal/repository/arangodb/arangodb_list_test.go
@@ -0,0 +1,102 @@
+package arangodb
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/dictyBase/modware-content/internal/model"
+	"github.com/dictyBase/modware-content/internal/repository"
+	"github.com/dictyBase/modware-content/internal/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestListContents(t *testing.T) {
+	t.Parallel()
+	assert, repo := setUp(t)
+	defer tearDown(repo)
+
+	// Create test data
+	createTestContents(assert, repo, 14)
+
+	// Test initial list
+	clist, err := repo.ListContents(0, 4, "")
+	assert.NoError(err, "expect no error from listing content")
+	assert.Len(clist, 5, "should have 5 content entries")
+	validateContentList(assert, clist)
+
+	// Test pagination
+	clist2, err := repo.ListContents(
+		clist[len(clist)-1].CreatedOn.UnixMilli(),
+		5,
+		"",
+	)
+	assert.NoError(err, "expect no error from listing content")
+	assert.Len(clist2, 6, "should have 6 content entries")
+	assert.Exactly(
+		clist[len(clist)-1],
+		clist2[0],
+		"should be identical content object",
+	)
+
+	// Test final page
+	clist3, err := repo.ListContents(
+		clist2[len(clist2)-1].CreatedOn.UnixMilli(),
+		7,
+		"",
+	)
+	assert.NoError(err, "expect no error from listing content")
+	assert.Len(clist3, 5, "should have 5 content entries")
+	assert.Exactly(
+		clist2[len(clist2)-1],
+		clist3[0],
+		"should be identical content object",
+	)
+}
+
+func createTestContents(
+	assert *require.Assertions,
+	repo repository.ContentRepository,
+	count int,
+) {
+	for i := 0; i < count; i++ {
+		time.Sleep(1 * time.Millisecond)
+		_, err := repo.AddContent(
+			testutils.NewStoreContent(fmt.Sprintf("gallery-%d", i), "genome"),
+		)
+		assert.NoErrorf(
+			err,
+			"expect no error from creating gallery genome content %s",
+			err,
+		)
+	}
+}
+
+func validateContentList(
+	assert *require.Assertions,
+	clist []*model.ContentDoc,
+) {
+	nrxp := regexp.MustCompile(`gallery-\d+`)
+	for idx, cnt := range clist {
+		assert.Equal(
+			cnt.Namespace,
+			"genome",
+			"should match the genome namespace",
+		)
+		assert.Regexp(nrxp, cnt.Name, "should match gallery names")
+		assert.Equal(
+			cnt.CreatedBy,
+			"content@content.org",
+			"should match the created_by",
+		)
+		if idx != 0 {
+			assert.Truef(
+				clist[idx-1].CreatedOn.After(clist[idx].CreatedOn),
+				"previous gallery record %s should be created after the current one %s",
+				clist[idx-1].CreatedOn.String(),
+				clist[idx].CreatedOn,
+			)
+		}
+	}
+}
--- internal/repository/arangodb/arangodb_test.go
+++ internal/repository/arangodb/arangodb_test.go
@@ -2,8 +2,6 @@ package arangodb
 
 import (
 	"encoding/json"
-	"fmt"
-	"regexp"
 	"strconv"
 	"testing"
 	"time"
@@ -87,92 +85,6 @@ func TestGetContentBySlug(t *testing.T) {
 	sct, err := repo.GetContentBySlug(nct.Slug)
 	assert.NoErrorf(err, "expect no error from getting content by slug %s", err)
 	testContentProperties(assert, sct, nct)
-}
-
-func TestListContents(t *testing.T) {
-	t.Parallel()
-	assert, repo := setUp(t)
-	defer tearDown(repo)
-
-	// Create test data
-	createTestContents(assert, repo, 14)
-
-	// Test initial list
-	clist, err := repo.ListContents(0, 4, "")
-	assert.NoError(err, "expect no error from listing content")
-	assert.Len(clist, 5, "should have 5 content entries")
-	validateContentList(assert, clist)
-
-	// Test pagination
-	clist2, err := repo.ListContents(
-		clist[len(clist)-1].CreatedOn.UnixMilli(),
-		5,
-		"",
-	)
-	assert.NoError(err, "expect no error from listing content")
-	assert.Len(clist2, 6, "should have 6 content entries")
-	assert.Exactly(
-		clist[len(clist)-1],
-		clist2[0],
-		"should be identical content object",
-	)
-
-	// Test final page
-	clist3, err := repo.ListContents(
-		clist2[len(clist2)-1].CreatedOn.UnixMilli(),
-		7,
-		"",
-	)
-	assert.NoError(err, "expect no error from listing content")
-	assert.Len(clist3, 5, "should have 5 content entries")
-	assert.Exactly(
-		clist2[len(clist2)-1],
-		clist3[0],
-		"should be identical content object",
-	)
-}
-
-func createTestContents(
-	assert *require.Assertions,
-	repo repository.ContentRepository,
-	count int,
-) {
-	for i := 0; i < count; i++ {
-		_, err := repo.AddContent(
-			testutils.NewStoreContent(fmt.Sprintf("gallery-%d", i), "genome"),
-		)
-		assert.NoErrorf(
-			err,
-			"expect no error from creating gallery genome content %s",
-			err,
-		)
-	}
-}
-
-func validateContentList(
-	assert *require.Assertions,
-	clist []*model.ContentDoc,
-) {
-	nrxp := regexp.MustCompile(`gallery-\d+`)
-	for idx, cnt := range clist {
-		assert.Equal(
-			cnt.Namespace,
-			"genome",
-			"should match the genome namespace",
-		)
-		assert.Regexp(nrxp, cnt.Name, "should match gallery names")
-		assert.Equal(
-			cnt.CreatedBy,
-			"content@content.org",
-			"should match the created_by",
-		)
-		if idx != 0 {
-			assert.True(
-				clist[idx-1].CreatedOn.After(clist[idx].CreatedOn),
-				"previous gallery record should be created after the current one",
-			)
-		}
-	}
 }
 
 func TestGetContent(t *testing.T) {
